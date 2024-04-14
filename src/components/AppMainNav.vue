a901d9d7addfec31f98a738c7566a62afd1537b0
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-14 22:33:58 +0200

    feat: add main-nav content

--- src/components/AppMainNav.vue
+++ src/components/AppMainNav.vue
@@ -5,15 +5,61 @@
 </script>
 
 <template>
-    <section>
-        <nav>
-            <ul>
-                <li></li>
-            </ul>
-        </nav>
+    <section class="main-nav">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                        <img src="../assets/img/buy-comics-digital-comics.png">
+                        <h3>DIGITAL COMICS</h3>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png">
+                        <h3>DC MERCHANDISE</h3>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png">
+                        <h3>SUBSCRIPTION</h3>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-shop-locator.png">
+                        <h3>COMIC SHOP LOCATOR</h3>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-dc-power-visa.svg">
+                        <h3>DC POWER VISA</h3>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/variables' as *;
 
+    .main-nav {
+        background-color: $brand-primary;
+        padding: 35px;
+
+        ul {
+            display: flex;
+            justify-content: space-around;
+            color: white;
+
+            li {
+                display: flex;
+                align-items: center;
+
+                img {
+                    height: 50px;
+                }
+
+                h3 {
+                    padding-left: 10px;
+                    font-size: 16px;
+                }
+            }
+        }
+    }
 </style>
